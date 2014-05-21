93d44ebdc8a0d24b491d70d5439eb85cec806acb
Ataias Pereira Reis <ataiasreis@gmail.com>
2014-05-21 18:44:17 -0400

    Added example of timing, now can compute timing of computation in microseconds range.

--- src/naviertest.cc
+++ src/naviertest.cc
@@ -1,5 +1,10 @@
 #include <FFH.hh>
 #include <navierstokes.hh>
+#include <chrono>
+
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+using std::chrono::steady_clock;
 
 int main(){
 	const int n = 5;
@@ -32,8 +37,16 @@ int main(){
 	MatrixXd fy(n,n);
 	fy = fx;
 
+	//Let's measure the time!
+	steady_clock::time_point start = steady_clock::now();
 	navier_stokes_step1(n, dt, mu, rho, &u, &v, &u_old, &v_old, &fx, &fy);
+	steady_clock::time_point end = steady_clock::now();
 
+	std::cout << "Computing took "
+          // duration_cast is required to avoid accidentally losing precision.
+          << duration_cast<microseconds>(end - start).count()
+          << "us.\n";
+          
 	cout << "\n u_old" << endl << u_old << endl;
 	cout << "\n v_old" << endl << v_old << endl;
 
