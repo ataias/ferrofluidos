6afac52aa477c882cef668833658e07485a30a1b
Ataias Pereira Reis <ataiasreis@gmail.com>
2014-09-16 16:52:58 -0300

    Correção de erros no trabalho, alguns sinais errados em fórmulas

--- include/navierstokes.hh
+++ include/navierstokes.hh
@@ -4,5 +4,12 @@
 void navier_stokes_step1(int n, double dt, double mu, double rho, 
 						 MatrixXd *u,     MatrixXd *v, 
 						 MatrixXd *u_old, MatrixXd *v_old, 
-						 MatrixXd *fx,    MatrixXd *fy);
+						 MatrixXd *fx,    MatrixXd *fy
+						 );
+
+void navier_stokes_step2(int n, double dt, double mu, double rho, 
+						 MatrixXd *p, //pressão 
+						 MatrixXd *u_star, MatrixXd *v_start //v*, obtido ignorando-se o termo envolvendo p
+						 );
+
 #endif
