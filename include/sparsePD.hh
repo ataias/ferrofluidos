98077468bf068a066c0a0dc173b1c3a4e08c5385
Ataias Pereira Reis <ataiasreis@gmail.com>
2014-12-12 19:15:05 -0200

    All c++ code was removed, including makefile. Project is now using julia

--- include/sparsePD.hh
+++
@@ -1,9 +0,0 @@
-#ifndef SPARSE_HH_
-#define SPARSE_HH_
-
-SpMat genSMPD(int n);
-MatrixXd gen_index(int n);
-VectorXd gen_b(MatrixXd B, MatrixXd F, int n);
-MatrixXd vectorToMatrix(MatrixXd B, VectorXd u, int n);
-MatrixXd exPoissonLDLT();
-#endif
