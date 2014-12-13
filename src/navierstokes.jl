0e79d6f1e865e6d086c796c0c3d9e2c4ca10601f
Ataias Pereira Reis <ataiasreis@gmail.com>
2014-12-13 14:46:57 -0200

    Bug in v* equation was corrected
    Code was also reviewed for errors for as long as I could.
    in v*, it was written u_old instead of v_old in one line

--- src/navierstokes.jl
+++ src/navierstokes.jl
@@ -9,26 +9,15 @@ export solve_navier_stokes!, testPoisson, isdXok, getDt
 function navier_stokes_step1!(n, dt, mu, rho, u, v, u_old, v_old, fx, fy, uB)
 	#u and v will be modified!!!
 	#int i, j; double u_s, v_s, u_t, v_t
-	dx = 1/(n-2)
+	dx = 1/(n-2.)
 	dx2 = dx*dx
 	dtx = dt/(2*dx)
-
-
-	#Boundary conditions, velocidades normais na malha escalonada
-	for j in 2:n-1
-		u[2,j]=0 #esquerda
-		u[n,j]=0 #direita
-	end
-
-	for i in 2:n-1
-		v[i,2]=0 #embaixo
-		v[i,n]=0 #em cima
-	end
 
 	#Process internal points
 	for i in 2:n-1
         #println("i is $(i)")
 		for j in 2:n-1
+
 			# Those two lines are just summing the stencil around i,j
 			u_s  = u_old[i+1,j]+u_old[i-1,j]+u_old[i,j+1]+u_old[i,j-1]
 			v_s  = v_old[i+1,j]+v_old[i-1,j]+v_old[i,j+1]+v_old[i,j-1]
@@ -45,23 +34,33 @@ function navier_stokes_step1!(n, dt, mu, rho, u, v, u_old, v_old, fx, fy, uB)
 			if i!=2 && i!=n #in the case i==2 or n, it is a boundary point
 				#Order is important here!
 				u[i,j]  =  mu*(u_s-4*u_old[i,j])/dx2+fx[i,j]
-				u[i,j] *= dt/rho
+				u[i,j] *= (dt/rho)
 				u[i,j] += -dtx*u_old[i,j]*(u_old[i+1,j]-u_old[i-1,j])
 				u[i,j] += -dtx*v_t*(u_old[i,j+1]-u_old[i,j-1])
 				u[i,j] +=  u_old[i,j]
 			end #if
 			#Now, let's compute v in i,j
-			if j!=2 && j!=n #in the case j==2 or n, it is a boundary point
+			if j!=2 && j!=n #in the case j==2, it is a boundary point
 				v[i,j]  =  mu*(v_s-4*v_old[i,j])/dx2+fy[i,j]
-				v[i,j] *= dt/rho
+				v[i,j] *= (dt/rho)
 				v[i,j] += -dtx*u_t*(v_old[i+1,j]-v_old[i-1,j])
 				v[i,j] += -dtx*v_old[i,j]*(v_old[i,j+1]-v_old[i,j-1])
-				v[i,j] += u_old[i,j]
+				v[i,j] += v_old[i,j]
 			end #if
 		end #for j
 	end #for i
-    println("u=", u)
-    println("v=", v)
+
+	#Boundary conditions, velocidades normais na malha escalonada
+	for j in 1:n
+		u[2,j] = 0 #esquerda
+		u[n,j] = 0 #direita
+	end
+
+	for i in 1:n
+		v[i,2] = 0 #embaixo
+		v[i,n] = 0 #em cima
+	end
+
 end #navier_stokes_step1
 
 function navier_stokes_step2!(n, dt, mu, rho, p, #pressão pode ser matriz zeros(6,6)
@@ -82,36 +81,32 @@ function navier_stokes_step2!(n, dt, mu, rho, p, #pressão pode ser matriz zeros
 		end
 	end
 
-	#Processar fronteira i=2
+	#Processar fronteira esquerda
 	i = 2
 	for j in 2:n-1
 		sum_aux = 2*u_old[i,j]-5*u_old[i+1,j]+4*u_old[i+2,j]-u_old[i+3,j]
-		f_aux = fx[i,j]+fx[i+1,j]
-		p[i-1,j] = p[i,j]-(mu/dx)*sum_aux-(rho*dx/2)*f_aux
+		p[i-1,j] = p[i,j] - (mu/dx)*sum_aux - rho*dx*fx[i,j]
 	end
 
-	#Processar fronteira i=n-1
-	i = n-1 #n é o tamanho da malha escalonada, o tamanho da malha de fato é n-1
+	#Processar fronteira i=n
+	i = n #n é o tamanho da malha escalonada
 	for j in 2:n-1
 		sum_aux = 2*u_old[i,j]-5*u_old[i-1,j]+4*u_old[i-2,j]-u_old[i-3,j]
-		f_aux = fx[i,j]+fx[i-1,j]
-		p[i+1,j] = p[i,j]+(mu/dx)*sum_aux+(rho*dx/2)*f_aux
+		p[i,j] = p[i-1,j] + (mu/dx)*sum_aux + rho*dx*fx[i,j]
 	end
 	
 	#Processar fronteira j=2
 	j = 2
 	for i in 2:n-1
 		sum_aux = 2*v_old[i,j]-5*v_old[i,j+1]+4*v_old[i,j+2]-v_old[i,j+3]
-		f_aux = fy[i,j]+fy[i,j+1]
-		p[i,j-1] = p[i,j]-(mu/dx)*sum_aux-(rho*dx/2)*f_aux
+		p[i,j-1] = p[i,j] - (mu/dx)*sum_aux - rho*dx*fy[i,j]
 	end
 
-	#Processar fronteira j=n-1
-	j = n-1 #n é o tamanho da malha escalonada, o tamanho da malha de fato é n-1
+	#Processar fronteira j=n
+	j = n #n é o tamanho da malha escalonada, o tamanho da malha de fato é n-1
 	for i in 2:n-1
 		sum_aux = 2*v_old[i,j]-5*v_old[i,j-1]+4*v_old[i,j-2]-v_old[i,j-3]
-		f_aux = fy[i,j]+fy[i,j-1]
-		p[i,j+1] = p[i,j]+(mu/dx)*sum_aux+(rho*dx/2)*f_aux
+		p[i,j] = p[i,j-1] + (mu/dx)*sum_aux + rho*dx*fy[i,j]
 	end
 end
 
@@ -119,7 +114,7 @@ function navier_stokes_step3!(n, dt, mu, rho, p, #pressão já calculada
 						 	  u, v, #u* e v*, serão atualizados para u(n+1)
 						 	  u_old, v_old, #u(n)
 						 	  fx, fy, uB)
-	dx = 1/(n-2)
+	dx = 1./(n-2)
 	px = 0.0
 	py = 0.0
 	#Pontos internos
@@ -137,6 +132,17 @@ function navier_stokes_step3!(n, dt, mu, rho, p, #pressão já calculada
 	for i in 2:n-1 u[i,1] = -u[i,2] end #embaixo
 	for j in 2:n-1 v[n,j] = -v[n-1,j] end #direita
 	for i in 2:n-1 u[i,n] = 2*uB[i]-u[i,n-1] end # em cima
+
+	#Boundary conditions, velocidades normais na malha escalonada
+	for j in 1:n
+		u[2,j] = 0 #esquerda
+		u[n,j] = 0 #direita
+	end
+
+	for i in 1:n
+		v[i,2] = 0 #embaixo
+		v[i,n] = 0 #em cima
+	end
 end
 
 function solve_navier_stokes!(n, dt, mu, rho, p, u, v, u_old, v_old, fx, fy, uB)
@@ -151,13 +157,14 @@ function solve_navier_stokes!(n, dt, mu, rho, p, u, v, u_old, v_old, fx, fy, uB)
 	i = 0
 	while error > threshold
 		navier_stokes_step2!(n, dt, mu, rho, p, u, v, u_old, v_old, fx, fy)
-		p[:,:] = p - minimum(p)
+		# p[:,:] = p - minimum(p)
+		p[:,:] = p - mean(p)
 		p[1,1] = 0; p[1,n]=0; p[n,1]=0; p[n,n]=0
 		error = maxabs(p-p_old)
 
 		p_old[:,:] = p
 		i = i + 1
-		if i == 12000 #limitado a 12 mil iterações... pode mudar este número
+		if i == 10000 #limitado a 12 mil iterações... pode mudar este número
 			return "Fim"
 		end
 	end
@@ -186,34 +193,30 @@ function testPoisson(n, dt, mu, p, rho, u, v, u_old, v_old, fx, fy)
 	i = 2
 	for j in 2:n-1
 		sum_aux = 2*u_old[i,j]-5*u_old[i+1,j]+4*u_old[i+2,j]-u_old[i+3,j]
-		f_aux = fx[i,j]+fx[i+1,j]
-		m[i-1,j] = p[i,j]-(mu/dx)*sum_aux-(rho*dx/2)*f_aux - p[i-1,j]
+		m[i-1,j] = - p[i-1,j] + p[i,j] - (mu/dx)*sum_aux - rho*dx*fx[i,j]
 	end
 
 	#Processar fronteira i=n-1
-	i = n-1 #n é o tamanho da malha escalonada, o tamanho da malha de fato é n-1
+	i = n #n é o tamanho da malha escalonada, o tamanho da malha de fato é n-1
 	for j in 2:n-1
 		sum_aux = 2*u_old[i,j]-5*u_old[i-1,j]+4*u_old[i-2,j]-u_old[i-3,j]
-		f_aux = fx[i,j]+fx[i-1,j]
-		m[i+1,j] = p[i,j]+(mu/dx)*sum_aux+(rho*dx/2)*f_aux - p[i+1,j]
+		m[i,j]  = - p[i,j] + p[i-1,j] + (mu/dx)*sum_aux + rho*dx*fx[i,j]
 	end
 	
 	#Processar fronteira j=2
 	j = 2
 	for i in 2:n-1
 		sum_aux = 2*v_old[i,j]-5*v_old[i,j+1]+4*v_old[i,j+2]-v_old[i,j+3]
-		f_aux = fy[i,j]+fy[i,j+1]
-		m[i,j-1] = p[i,j]-(mu/dx)*sum_aux-(rho*dx/2)*f_aux - p[i,j-1]
+		m[i,j-1] = - p[i,j-1] + p[i,j] - (mu/dx)*sum_aux - rho*dx*fy[i,j]
 	end
 
 	#Processar fronteira j=n-1
-	j = n-1 #n é o tamanho da malha escalonada
+	j = n #n é o tamanho da malha escalonada
 	for i in 2:n-1
 		sum_aux = 2*v_old[i,j]-5*v_old[i,j-1]+4*v_old[i,j-2]-v_old[i,j-3]
-		f_aux = fy[i,j]+fy[i,j-1]
-		m[i,j+1] = p[i,j]+(mu/dx)*sum_aux+(rho*dx/2)*f_aux - p[i,j+1]
+		m[i,j] = - p[i,j] + p[i,j-1] + (mu/dx)*sum_aux + rho*dx*fy[i,j]
 	end
-	println(m)
+	# println(m)
 	return maxabs(m)
 end
 
@@ -230,7 +233,7 @@ function isdXok(Re, n)
 end
 
 function getDt(n, Re, divFactor=5)
-	dx = 1/(n-2) #n é o tamanho da malha escalonada, por isso está n-2 ao invés de n-1
+	dx = 1./(n-2) #n é o tamanho da malha escalonada, por isso está n-2 ao invés de n-1
 	dt1 = 0.25*dx*dx/Re
 	dt2 = dx
 	dt = 0.0
@@ -239,7 +242,6 @@ function getDt(n, Re, divFactor=5)
 	else 
 		dt = dt2/divFactor
 	end
-	println("dt = ", dt);
 	return dt;
 end
 
--- src/naviertest.jl
+++ src/naviertest.jl
@@ -1,27 +1,39 @@
 using NavierStokes
 #need to execute this directly... otherwise those variables are all local
-n = 6;
+n = 20;
+dx = 1/(n-2)
 p = zeros(n,n);
 u = zeros(n,n);
-u[:,n] = 2*ones(typeof(1.0),1,n)
-println(u)
+uB = zeros(1,n);
+for i in -1:n-2
+	uB[i+2] = (sin(pi*i*dx))^2
+	#uB[i+2] = 1
+end
+u[:,n] = 2*uB
+u[1,:] = NaN
+	
 v = zeros(n,n);
+v[:,1] = NaN
+
 u_old = copy(u);
 v_old = copy(v);
 fx = zeros(n,n);
 fy = zeros(n,n);
-mu = 1.0;
+mu = 0.1;
 rho = 1.0;
 Re = 1.0/mu;
-# uB = [1.0 1 1 1 1 1];
-uB = ones(typeof(1.0),1,n)
-dt = getDt(n, Re);
+dt = getDt(n, Re, 1.5);
 
 
 if(!isdXok(1/mu, n))
 	println("There is a problem with your dx. Increase n.\n");
 	exit(0)
 end
-@time solve_navier_stokes!(n, dt, mu, rho, p, u, v, u_old, v_old, fx, fy, uB)
-println(u)
-println(v)
+
+@time for i in 1:2500
+	solve_navier_stokes!(n, dt, mu, rho, p, u, v, u_old, v_old, fx, fy, uB)
+	u, u_old = u_old, u
+	u[:,:] = u_old[:,:]
+end
+shouldBeZero = sum(u[2,:])+sum(u[n,:])+sum(v[:,2])+sum(u[:,n])
+println("sum(u[2,:])+sum(u[n,:])+sum(v[:,2])+sum(u[:,n])=$(shouldBeZero)")
