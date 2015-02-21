9a478a4746da29dc3c85bff82b809f9c3119ee58
Ataias Pereira Reis <ataiasreis@gmail.com>
2015-02-21 09:48:59 -0200

    Changed folder location of Poisson with Neumann conditions

--- src/poissonDirichletSparse.jl
+++ src/poissonDirichletSparse.jl
@@ -1,3 +1,6 @@
+module PoissonDirichlet
+
+#Este módulo não considera que a malha esteja escalonada
 function poissonDirichletSparseSolver(n, p, f, A, K, left, right, upper, lower)
     spn = (n-2)*(n-2) #sparse n
     dx = 1/(n-1)
@@ -76,32 +79,4 @@ function getK(n)
     return K
 end
 
-function test(n = 101)
-    p = zeros(n,n)
-    f = zeros(n,n); #valor da f também no meio da célula
-    left = zeros(1,n);
-    right = zeros(1,n);
-    upper = zeros(1,n);
-    lower = zeros(1,n);
-    dx = 1/(n-1)
-    for i in 1:n
-        x = (i-1)*dx;
-        upper[i] = sinpi(x);
-    end
-    
-    A = getA(n)
-    K = getK(n)
-    poissonDirichletSparseSolver(n, p, f, A, K, left, right, upper, lower)
-    
-    p_solucao = zeros(n,n);
-    for i in 1:n
-        for j in 1:n
-            #pressão avaliada no centro da célula
-            x = (i-1)*dx;
-            y = (j-1)*dx;
-            p_solucao[i,j] = (sinh(pi*y)/(sinh(pi)))*sinpi(x);
-        end
-    end
-    
-    println("Error = ", maximum(abs(p_solucao-p)))
 end
