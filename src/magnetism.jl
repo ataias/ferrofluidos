0633c960546107719209ea9ab6698e00b6b137b1
Ataias Pereira Reis <ataiasreis@gmail.com>
2015-04-08 15:08:21 -0300

    Função para calcular ângulo entre duas matrizes e para obter matriz de rotação foram adicionadas.

--- src/magnetism.jl
+++ src/magnetism.jl
@@ -36,10 +36,31 @@ function rotInside(Fx, Fy, n)
     return rotMax
 end
 
+# Calcula ângulo entre dois vetores para as matrizes H e M
+#cada ponto deve ser um número complexo
+function Angle(a, b)
+    angles = zeros(size(a,1), size(a,2));
+    for i in 1:size(a, 1)
+        for j in 1:size(a,2)
+            angles[i,j] = angle(reim(a[i,j]),reim(b[i,j]))
+        end
+    end
+    return angles;
+end
+
+function angle(a, b)
+  return acosd(dot(a,b)/norm(a)/norm(b))
+end
+
+function angle(a::(Number,Number), b::(Number,Number))
+    return angle([a[1], a[2]], [b[1], b[2]])
+end
+
+
 function H0(Br, mu_m, L0, a, b, theta, x0, y0)
     K = (Br/pi/mu_m)
     H0x = x -> K*(atan(a*b/(x*sqrt(a^2+b^2+x^2))) - atan(a*b/((x+L0)*sqrt(a^2+b^2+(x+L0)^2))))
-    
+
     Hx = (x,y) -> H0x(x*cos(theta)+y*sin(theta))*cos(theta)
     Hy = (x,y) -> H0x(x*cos(theta)+y*sin(theta))*sin(theta)
     return ((x,y) -> Hx(x-x0,y-y0)), ((x,y) -> Hy(x-x0,y-y0))
@@ -48,7 +69,7 @@ end
 #fHx and fHy are functions
 function getPhi!(n, phi, Mx, My, fHx, fHy, A)
     dx = 1/(n-2)
-    
+
     #f = div M
     f = zeros(n,n); #valor de f no meio da célula
     for i in 2:n-1
@@ -56,12 +77,12 @@ function getPhi!(n, phi, Mx, My, fHx, fHy, A)
             f[i,j] = (Mx[i+1,j] - Mx[i,j])/dx + (My[i,j+1] - My[i,j])/dx
         end
     end
-    
+
     left = zeros(n);
     right = zeros(n);
     upper = zeros(n);
     lower = zeros(n);
-    
+
     for i in -1:n-2
       x = (i+0.5)*dx;
       left[i+2]  = Mx[2,i+2] - fHx(0,x)
@@ -71,7 +92,7 @@ function getPhi!(n, phi, Mx, My, fHx, fHy, A)
     end
     #Solve for p with Neumann conditions here
     poissonNeumannSparseSolver(n, phi, f, A, left, right, upper, lower)
-    
+
 end #end getPhi!
 
 function getMH!(n, chi, phi, Mx, My, Hx, Hy)
@@ -82,19 +103,19 @@ function getMH!(n, chi, phi, Mx, My, Hx, Hy)
             Hy[i,j] = -(phi[i,j] - phi[i,j-1])/dx
         end
     end
-    
+
     for j in 2:n-1
         Hy[1,j] = -(phi[1,j] - phi[1,j-1])/dx
         Hy[n,j] = -(phi[n,j] - phi[n,j-1])/dx
     end
-    
+
     for i in 2:n-1
         Hx[i,1] = -(phi[i,1] - phi[i-1,1])/dx
         Hx[i,n] = -(phi[i,n] - phi[i-1,n])/dx
     end
-    
-#    Mx[:,:] = chi*Hx[:,:]
-#    My[:,:] = chi*Hy[:,:]
+
+    Mx[:,:] = chi*Hx[:,:]
+    My[:,:] = chi*Hy[:,:]
 end #end getMH!
 
 
@@ -102,18 +123,18 @@ function getForce!(n, Cpm, Hx, Hy, Mx, My, fx, fy)
     dx = 1/(n-2)
     Myt = 0.0
     Mxt = 0.0
-    
+
     for i in 2:n-1
         for j in 2:n-1
             Myt = (My[i,j]+My[i,j+1]+My[i-1,j]+My[i-1,j+1])/4
             fx[i,j]  = Cpm*Mx[i,j]*( Hx[i,j+1]-Hx[i,j-1])/(2*dx)
             fx[i,j] += Cpm*Myt*(Hx[i+1,j]-Hx[i-1,j])/(2*dx)
-            
+
             Mxt = (Mx[i,j]+Mx[i+1,j]+Mx[i,j-1]+Mx[i+1,j-1])/4
             fy[i,j]  = Cpm*Mxt*(Hy[i+1,j]-Hy[i-1,j])/(2*dx)
             fy[i,j] += Cpm*My[i,j]*( Hy[i,j+1]-Hy[i,j-1])/(2*dx)
         end
-    end 
+    end
 
 end #end getForce!
 
@@ -122,27 +143,33 @@ function solve!(n = 7)
     phi = zeros(n,n);
     Mx = zeros(n,n);
     My = zeros(n,n);
-#    
+#
     fMx = (x,y) -> x
     fMy = (x,y) -> -y
-    
+
+    theta = pi/4
+    R = theta -> [cos(theta) -sin(theta); sin(theta) cos(theta)]
+    Rt = R(theta)
+
     for i in -1:n-2
         x = (i+0.5)*dx;
         for j in -1:n-2
             y = (j+0.5)*dx;
-            Mx[i+2,j+2] = fMx(x,y)
-            My[i+2,j+2] = fMy(x,y)
+            xl, yl = Rt'*[x y]'
+            Mx[i+2,j+2] = (Rt*[fMx(xl,yl) fMy(xl,yl)]')[1]
+            My[i+2,j+2] = (Rt*[fMx(xl,yl) fMy(xl,yl)]')[2]
         end
     end
-    
+    #Quando for calcular phi, devo fazer M = chi*H primeiro? Isso terá influência no phi por meio de suas condições de contorno.
+
     A = getANeumannSparse(n);
     getPhi!(n, phi, Mx, My, (x,y) -> 1, (x,y) -> 1, A);
-    
+
     chi = 1
     Hx = zeros(n,n);
     Hy = zeros(n,n);
     getMH!(n, chi, phi, Mx, My, Hx, Hy);
-    
+
     hxn = zeros(n-2, n-2); hyn = zeros(n-2, n-2);
     mxn = zeros(n-2, n-2); myn = zeros(n-2, n-2);
     phin = zeros(n-2, n-2);
@@ -155,14 +182,14 @@ function solve!(n = 7)
     write(file, hyn)
     write(file, phin)
     close(file)
-    
+
     print("Max div B = ", divInside(mxn+hxn, myn+hyn, n-2), "\n")
     print("Max rot H = ", rotInside(hxn, hyn, n-2), "\n")
     Cpm = 10;
     fx = zeros(n,n);
     fy = zeros(n,n);
     getForce!(n, Cpm, Hx, Hy, Mx, My, fx, fy);
-    
+
 end
 
 end
