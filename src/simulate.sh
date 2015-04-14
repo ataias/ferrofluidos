6c08f15be9b6bacc41e121fb826a29bb568235fd
Ataias Pereira Reis <ataiasreis@gmail.com>
2015-04-14 11:30:42 -0300

    Bash script to simulate and generate video

---
+++ src/simulate.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#Faz simulação e gera vídeo para centro do campo magnético em (a,b)
+#0 <= a <= 1, mesmo para b
+simul () {
+a=$1
+b=$2
+FOLDERTOSAVE=$3
+VIDEONAME=$4 #no extension
+rm -rf $FOLDERTOSAVE
+mkdir -p $FOLDERTOSAVE
+
+#Parâmetros
+n=22; #tamanho da malha
+t=2.5;
+Re=10;
+divFactor=1.25;
+chi=0.5;
+Cpm=0.8;
+save=1; #para salvar o arquivo de simulação, deve ser setado em 1
+
+FILENAME=N$((n - 2)).dat
+
+step=2; #para plotar png
+
+#simulação
+julia frames.jl $n $t $Re $divFactor $chi $Cpm $save $a $b > out$((n-2)).txt
+rm -rf png
+mkdir -p png && cd png
+mv -v ../$FILENAME .
+../vectorField.py $((n-2)) $t $step $chi $Cpm $Re
+mv -v $FILENAME $FOLDERTOSAVE/
+cd ..
+mv -v out$((n-2)).txt $FOLDERTOSAVE/
+mv -v png $FOLDERTOSAVE/png
+ffmpeg -i $FOLDERTOSAVE/png/%4d.png -c:v libx264 -vf fps=30 -pix_fmt yuv420p $FOLDERTOSAVE/$VIDEONAME.mp4
+}
+
+simul 0.0 0.5 /Users/ataias/Documents/ferrisimulacao/sim01 sim01
