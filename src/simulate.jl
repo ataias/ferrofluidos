fd1aa959b2e36305f3177704d0cadbe3730d4784
Ataias Pereira Reis <ataiasreis@gmail.com>
2015-05-03 10:16:25 -0300

    Simulate.jl only generates .dat file in proper folder, no more png generation Image generation will be done in another script

--- src/simulate.jl
+++ src/simulate.jl
@@ -1,6 +1,10 @@
 using Transient
 using NavierTypes
 
+#Executar
+#julia -p 1 simulate.jl 13 &
+#-p 1 -> indica que julia deve utilizar somente um processo
+
 t = 3.0;
 Re = vcat(1,10:10:100)
 #n = map(x -> (x + 10) - (x+10) % 10, Re + 20) + 2 #se quiser ter malhas diferentes...
@@ -24,42 +28,39 @@ main = homedir()*"/Documents/simulacao"
 function simulation(n, dt, Re, chi, Cpm, gamma, a, b, mag)
   folder = string(mag) * "Re" * string(int(Re)) * "N" * string(n-2)
   filename = folder * ".txt"
+    
+  #Create working folder
+  wfolder = main * "/" * folder #working folder
+  try rm(wfolder, recursive=true) end
+  mkdir(wfolder)
+  cd(wfolder)
+  
+  #redirect standard output
   f = open(filename, "w")
   redirect_stdout(f)
+    
+  #Simulate
   @time transient(n, dt, Re, t, chi, Cpm, gamma, a, b, save)
-    #File operations
-  try rm(main * "/" * folder, recursive=true) end
-  mkdir(main * "/" * folder)
-  try rm("png", recursive=true) end
+  
+  #File operations
   mkdir("png")
   cd("./png")
   simulfile =  "N"*string(n - 2)*".dat"
-  mv("../" * simulfile, "./" * simulfile)
-  run(`../vectorField.py $((n-2)) $t $step $chi $Cpm $Re $gamma`)
-  mv(simulfile, main*"/"*folder*"/"*simulfile)
+  mv("../"*simulfile, "./"*simulfile)
   cd("..")
+  println("\n Execute o script python de dentro da pasta png")
   close(f)
-  mv(filename, main*"/"*folder*"/"*filename)
-  mv("png", main*"/"*folder*"/png")
 end
 
 s = int(ARGS[1]) #simulation number
-
-#for i in 1:size(n,1)
-#  simulation(n[i], dt[i], Re[i]*1.0, 0.0, 0.0, 0.0, a, b, "nomag") #without magnetism
-#end
-#
-#for i in 1:size(n,1)
-#  simulation(n[i], dt[i], Re[i]*1.0, 0.5, 0.8, 3.5, a, b, "mag") #with magnetism
-#end
 
 #Há 11 simulações de cada tipo
 #Argumentos entre 1 e 11 realizam as simulações nomag
 #Argumentos entre 12 e 22 realizam as simulações mag
 if 1 <= s <= size(Re,1)
-    simulation(n[s], dt[s], Re[s]*1.0, 0.0, 0.0, 0.0, a, b, "nomag")
+    simulation(n[s], dt[s], Re[s]*1.0, 0.0, 0.0, 0.0, a, b, "n") #no magnetism
 elseif s <= int(2*size(Re,1))
-    simulation(n[s%11], dt[s%11], Re[s%11]*1.0, 0.5, 0.8, 3.5, a, b, "mag")
+    simulation(n[s%11], dt[s%11], Re[s%11]*1.0, 0.5, 0.8, 3.5, a, b, "w") #with magnetism
 else
     println("Invalid simulation number")
 end
