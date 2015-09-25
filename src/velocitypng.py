1202f878237632b7b5ad256003821cfa344f9fef
Ataias Pereira Reis <ataiasreis@gmail.com>
2015-09-25 11:40:17 -0300

    Agora o programa cria as imagens para a evolução da magnetização também

--- src/velocitypng.py
+++ src/velocitypng.py
@@ -42,13 +42,21 @@ if __name__ == "__main__":
 
         #Parâmetros derivados
         dx = 1.0/n
-        def sideText(time):
+        def sideTextV(time):
             text(1.21, 1.0, r'Re = ' + str(Re))
             text(1.21, 0.95, r'$\alpha = ' + str(alpha) + '$')
             text(1.21, 0.9, r'Cpm = ' + str(Cpm))
             text(1.21, 0.85, r'$n = ' + str(n) + '$')
             text(1.21, 0.8, r'$\mathrm{Pe} = ' + str(Pe) + '$')
             title(r'$\mathbf{v}$, ' + 't = ' + '{:10.8f}'.format(time))
+
+        def sideTextM(time):
+            text(1.21, 1.0, r'Re = ' + str(Re))
+            text(1.21, 0.95, r'$\alpha = ' + str(alpha) + '$')
+            text(1.21, 0.9, r'Cpm = ' + str(Cpm))
+            text(1.21, 0.85, r'$n = ' + str(n) + '$')
+            text(1.21, 0.8, r'$\mathrm{Pe} = ' + str(Pe) + '$')
+            title(r'$\mathbf{M}$, ' + 't = ' + '{:10.8f}'.format(time))
 
         u = zeros((n,n), dtype=float64)
         v = zeros((n,n), dtype=float64)
@@ -73,12 +81,22 @@ if __name__ == "__main__":
         for i in range(0, numberFrames):
             time = (i)/180.0
             try:
+                # batchRead(u, v, p, Hx, Hy, Mx, My, phi, angles, n, f)
+                #1 = SEEK_CUR -> offset relative to current position
+
                 readMatrix(u, f, n)
                 readMatrix(v, f, n)
+                plotStreamFrame(u, v, x, y, n, sideTextV, time, "v" + str(i).zfill(4) + ".png")
 
-                #1 = SEEK_CUR -> offset relative to current position
-                f.seek(n*n*8*7, 1)
-                plotStreamFrame(u, v, x, y, n, sideText, time, str(i).zfill(4) + ".png")
+                f.seek(n*n*8*3, 1)
+
+                readMatrix(Mx, f, n)
+                readMatrix(My, f, n)
+                plotStreamFrame(Mx, My, x, y, n, sideTextM, time, "M" + str(i).zfill(4) + ".png")
+
+                f.seek(n*n*8*2, 1)
+
+
             except:
                 print("Exception occurred!")
                 break
